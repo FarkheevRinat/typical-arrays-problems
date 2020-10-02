b92c204c389e713cc0b9819ac6208d0674402380
Farkheev Rinat <frinat04051985@gmail.com>
2020-10-02 18:58:22 +0500

    init: start task 'Typical Arrays Problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    let result;
+
+    if (array && array.length > 0) {
+        array.sort((a, b) => a - b);
+
+        result = array[0];
+    } else {
+        result = 0;        
+    }
+
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result;
+
+    if (array && array.length > 0) {
+        array.sort((a, b) => a - b);
+
+        result = array[array.length - 1];
+    } else {
+        result = 0;        
+    }
+
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result;
+
+    if (array && array.length > 0) {
+        result = array.reduce((a, b) => a + b, 0) / array.length;
+    } else {
+        result = 0;        
+    }
+
+    return result;
 }
